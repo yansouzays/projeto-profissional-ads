4ae3199babf9e0db7d42eac41303f8e83f9a0561
yansouzays <yancfa@gmail.com>
2026-09-03 23:37:24 -0300

    atualizaçao de funções

---
+++ Funçoes.py
@@ -0,0 +1,28 @@
+def RegistrarClientes(clientes: list):
+    nome = input("Digite o nome do cliente: ").upper().split()
+    telefone = int(input("Digite o telefone do cliente: "))
+    cpf = input("Digite o cpf do cliente: ").split()
+    client = {
+        "nome": nome,
+        "telefone": telefone,
+        "cpf": cpf
+    }
+    clientes.append(client)
+clientes = []
+RegistrarClientes(clientes) 
+RegistrarClientes(clientes)
+print(clientes)   
+
+
+def RegistrarEquipamentos(equipamentos: list):
+    equipamento = input("Digite o nome do equipamento: ")
+    quantidade = input("Digite a quantidade disponivel desse equipamento: ")
+    equip = {
+        "equipamento": equipamento,
+        "quantidade": quantidade 
+    }
+    equipamentos.append(equip)
+equipamentos = []
+RegistrarEquipamentos(equipamentos)
+RegistrarEquipamentos(equipamentos)
+print(equipamentos)
