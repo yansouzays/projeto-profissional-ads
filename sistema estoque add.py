eceb9f962a14faa671c46034ca63c66c48e3b329
loranahs-source <lorana.hs@gmail.com>
2026-09-03 21:25:38 -0300

    cadastra todos os 8 IDs no estoque e ajusta chamada de funcao

--- sistema estoque add.py
+++ sistema estoque add.py
@@ -114,3 +114,5 @@ def gerenciar_estoque_manual():
 
     peca_encontrada[quantidade]-= quantidade
     print(f"removidas{quantidade} unidades. Novo saldo de '{peca_encontrada['nome']}' : {peca_encontrada[quantidade]}")
+
+gerenciar_estoque_manual()
